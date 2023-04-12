8673c8ef94b536799631bcbcfa4e6806ff40b3a4
Phu Le <lenguyenvantanphu@gmail.com>
2023-04-12 14:34:00 +0700

    Change all

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const config = require('./common/config/env.config.js');
+
+const express = require('express');
+const app = express();
+
+const AuthorizationRouter = require('./authorization/routes.config');
+const UsersRouter = require('./users/routes.config');
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Expose-Headers', 'Content-Length');
+    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    } else {
+        return next();
+    }
+});
+
+app.use(express.json());
+AuthorizationRouter.routesConfig(app);
+UsersRouter.routesConfig(app);
+
+
+app.listen(config.port, function () {
+    console.log('app listening at port %s', config.port);
+});
